4a7ce95cf61e3b23e7ae5ecfd3436de79b4c5518
Japamn <chap.ngne@gmail.com>
2024-06-23 10:07:50 +0700

    init project

---
+++ demoGitHub/demoGitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoGitHub
+//
+//  Created by areeya on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoGitHub/demoGitHub/demoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGitHubApp.swift
+//  demoGitHub
+//
+//  Created by areeya on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
